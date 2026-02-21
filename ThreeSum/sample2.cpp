d259e0476c00318fdbc157f7595d0ac6471dd663
vigneshrajap <vignesh.raja199@gmail.com>
2026-02-21 12:04:17 +0100

    ThreeSum

---
+++ ThreeSum/sample2.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        vector<vector<int>> result;
+        sort(nums.begin(), nums.end()); // Sorting is key!
+
+        for (int i = 0; i < nums.size(); i++) {
+            // Skip the same element to avoid duplicate triplets
+            if (i > 0 && nums[i] == nums[i - 1]) continue;
+
+            int left = i + 1;
+            int right = nums.size() - 1;
+
+            while (left < right) {
+                int sum = nums[i] + nums[left] + nums[right];
+
+                if (sum == 0) {
+                    result.push_back({nums[i], nums[left], nums[right]});
+                    
+                    // Move pointers and skip duplicates
+                    while (left < right && nums[left] == nums[left + 1]) left++;
+                    while (left < right && nums[right] == nums[right - 1]) right--;
+                    
+                    left++;
+                    right--;
+                } else if (sum < 0) {
+                    left++; // Need a larger value
+                } else {
+                    right--; // Need a smaller value
+                }
+            }
+        }
+        return result;
+    }
+};
