885552d7da4c2678e5cb4fad4a95cc71e467ce42
vigneshrajap <vignesh.raja199@gmail.com>
2026-07-26 22:14:24 +0200

    DecodeXORArray

---
+++ DecodeXORArray/sample.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    vector<int> decode(vector<int>& encoded, int first) {
+        
+        vector<int> result;
+        result.push_back(first);
+
+        for(int i=0; i<encoded.size();i++){
+            result.push_back(result[i]^encoded[i]);
+        }
+
+        return result;
+    }
+};
